86e32067c75b9728b14163ee0a3931fb9451d542
forhadbinamir <forhadbinamir@gmail.com>
2022-05-26 08:02:59 +0300

    create payment page

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,9 +1,20 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
-
+import CheckoutForm from './CheckoutForm';
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 const Payment = () => {
     return (
-        <div>
-            <h2>payment</h2>
+        <div className='flex justify-center'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title">Card title!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
+                </div>
+            </div>
         </div>
     );
 };
